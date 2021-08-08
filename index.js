0fbc070301bbbd0f874ae67949b067708f5f3496
GosWoj <gosiawoojcik@gmail.com>
2021-08-08 15:40:40 +0200

    step9 done - making backened work with frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 let persons = [
@@ -31,6 +32,7 @@ morgan.token("person", function getPerson(req) {
 
 app.use(express.json());
 app.use(morgan(":method :url :status :response-time :person"));
+app.use(cors());
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
