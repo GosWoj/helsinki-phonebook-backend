e10d9cc3cace22d7e7307cf8644b7462c3d2489c
GosWoj <gosiawoojcik@gmail.com>
2021-08-11 14:35:00 +0200

    database step3 - person is now deleted from database

--- index.js
+++ index.js
@@ -5,37 +5,14 @@ const cors = require("cors");
 const Person = require("./models/person");
 const app = express();
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 morgan.token("person", function getPerson(req) {
   return JSON.stringify(req.person);
 });
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan(":method :url :status :response-time :person"));
 app.use(cors());
-app.use(express.static("build"));
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((result) => {
@@ -55,10 +32,14 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id)
+    .then((person) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(404).end();
+    });
 });
 
 app.post("/api/persons", (request, response) => {
